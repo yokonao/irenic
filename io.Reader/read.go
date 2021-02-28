281a0ea0b51c4090190952e40df2e5f88ba8b115
yokonao <yokotukanao@me.com>
2021-03-01 01:26:20 +0900

    PNGファイルの読み込みを実装

--- io.Reader/read.go
+++ io.Reader/read.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	readBigEndian()
+	readPNG()
 }
 
 func readBigEndian() {
@@ -18,4 +21,41 @@ func readBigEndian() {
 	// 試しにリトルエンディアン
 	binary.Read(bytes.NewReader(data), binary.LittleEndian, &i)
 	fmt.Printf("data: %d\n", i)
+}
+
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("length: %d, chunk: %v\n", length, string(buffer))
+}
+
+func readChunks(file *os.File) []io.Reader {
+	var chunks []io.Reader
+
+	file.Seek(8, 0)
+	var offset int64 = 8
+	for {
+		var length int32
+		err := binary.Read(file, binary.BigEndian, &length)
+		if err == io.EOF {
+			break
+		}
+		chunks = append(chunks, io.NewSectionReader(file, offset, int64(length)+12))
+		offset, _ = file.Seek(int64(length)+8, 1)
+	}
+	return chunks
+}
+
+func readPNG() {
+	file, err := os.Open("resource/lena.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	chunks := readChunks(file)
+	for _, chunk := range chunks {
+		dumpChunk(chunk)
+	}
 }
